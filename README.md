4607fa4693be240f97dd32ae6124190aa6e1b72a
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-08-28 22:58:16 +0700

    Progate SQL Study 4

---
+++ README.md
@@ -0,0 +1,3 @@
+#Progate Belajar SQL
+
+<h2>SQL Study 4(Menambah, Mengubah dan Menghapus Data)</h2>
---
+++ page3/exercise1.sql
@@ -0,0 +1,4 @@
+-- Hapus data yang ber-id 7 di tabel student
+DELETE FROM students WHERE id=7;
+-- Jangan hapus kueri dibawah
+SELECT * FROM students;
